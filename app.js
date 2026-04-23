00ba98a68c5eb26a71729feda1a31b5d0136315d
OPI SOPIAH <opisopiah@MacBook-Air-OPI.local>
2026-04-23 09:19:15 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('XXXXXini otomatis Hello dari local DevOps UPDATE');
+  res.end('mantap boss');
 });
 
 server.listen(3000, "0.0.0.0", () => {
